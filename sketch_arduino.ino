c9fa03c71789eb926ee6cc2415b9981adf9b6f8f
Simon Jamain <jamain.simon@gmail.com>
2018-05-12 20:34:11 +0200

    working bluetooth over software serial

--- sketch_arduino.ino
+++ sketch_arduino.ino
@@ -26,16 +26,24 @@ X: B01110110
 Y: B01101110
 Z: B01011011
 */
+
+#define USE_SOFTWARE_SERIAL
+//#undef USE_SOFTWARE_SERIAL
+
 #include <TM1637.h>
 #include <Pushbutton.h>
 #include <EEPROM.h>
+#ifdef USE_SOFTWARE_SERIAL
+#include <SoftwareSerial.h>
+#define SERIAL_OBJECT hc06
+#else
+#define SERIAL_OBJECT Serial
+#endif
 #include <Cmd.h>
 
-#define TESTING
+//#define TESTING
+#define MULTIMODE_BUTTON
 //#define EEPROM_INITIALIZATION
-
-#define DISPLAY_CLK 12
-#define DISPLAY_DIO 11
 
 #define EEPROM_VOLTAGE_PROTECTION_CUTOFF_ADRESS 0
 #define EEPROM_VOLTAGE_PROTECTION_ALERT_ADRESS 4
@@ -43,6 +51,8 @@ Z: B01011011
 #define EEPROM_FIREMODE_1_CYCLE_ADRESS 9
 #define EEPROM_FIREMODE_2_ROUNDS_NUMBER_ADRESS 10
 #define EEPROM_FIREMODE_2_CYCLE_ADRESS 11
+#define EEPROM_PRECOCK_AFFINE_OFFSET_VOLTAGE 12
+#define EEPROM_PRECOK_AFFINE_SLOPE 16
 
 #define STATE_IDLE_HOT 0
 #define STATE_IDLE_SAFE 1
@@ -51,16 +61,30 @@ Z: B01011011
 
 #define CURRENT_FIREMODE_1 1
 #define CURRENT_FIREMODE_2 2
+#ifdef MULTIMODE_BUTTON
+#define CURRENT_FIREMODE_SAFE 3
+#endif
 
 #define FIREMODE_CYCLE_CONFIG_STOP_IMMEDIATELY_ON_RELEASE 0
 #define FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE 1
 #define FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE_AND_KEEP_ON_PRESS 2
 
+
 #define TRIGGER_BUTTON_PIN 2
 #define TAPPET_PLATE_SENSOR_PIN 3
-#define SAFETY_SENSOR_PIN 5
-#define MODE_1_SENSOR_PIN 6
-#define MODE_2_SENSOR_PIN 7
+
+#ifdef MULTIMODE_BUTTON
+#define MULTIMODE_BUTTON_PIN 5
+#else
+#define SAFETY_SENSOR_PIN 
+#define MODE_1_SENSOR_PIN 
+#define MODE_2_SENSOR_PIN 
+#endif
+
+#define DISPLAY_DIO_PIN 6
+#define DISPLAY_CLK_PIN 7
+#define BLUETOOTH_RX_PIN 11
+#define BLUETOOTH_TX_PIN 12
 
 #define MOTOR_PIN 10
 #define VOLTAGE_SENSOR_PIN A0
@@ -68,24 +92,29 @@ Z: B01011011
 #define VOLTAGE_CORRECTION_FACTOR 1.0
 #define DISPLAY_REFRESH_TIME_MILLISECONDS 100
 #define DISPLAY_GLOBAL_DEBOUNCE_TIMER 300
+#define PRECOCK_DELAY 40
 
 uint8_t state;
 boolean eventTestingMode;
 uint8_t currentFiremode;
-uint8_t* currentFiremodeRoundsNumberConfig;
-uint8_t* currentFiremodeCycleConfig;
+uint8_t *currentFiremodeRoundsNumberConfig;
+uint8_t *currentFiremodeCycleConfig;
 uint8_t shotsLeft;
 boolean fullAuto;
 boolean cocked;
 
-TM1637 display(DISPLAY_CLK, DISPLAY_DIO);
+TM1637 display(DISPLAY_CLK_PIN, DISPLAY_DIO_PIN);
 
 unsigned long timeOnLastDisplay;
 
 Pushbutton triggerButton(TRIGGER_BUTTON_PIN);
+#ifdef MULTIMODE_BUTTON
+Pushbutton multimodeButton(MULTIMODE_BUTTON_PIN);
+#else
 Pushbutton mode1SensorButton(MODE_1_SENSOR_PIN);
 Pushbutton mode2SensorButton(MODE_2_SENSOR_PIN);
 Pushbutton safetySensorButton(SAFETY_SENSOR_PIN);
+#endif
 Pushbutton tappetPlateSensorButton(TAPPET_PLATE_SENSOR_PIN, PULL_UP_ENABLED, DEFAULT_STATE_LOW);
 
 //config variables
@@ -103,7 +132,6 @@ void enterSTATE_IDLE_HOT()
   Serial.println("STATE_IDLE_HOT");
 #endif
 
-  display.displayRaw((const uint8_t[]){B01110110, B00111111, B01111000, B00000000});
   state = STATE_IDLE_HOT;
 }
 
@@ -113,8 +141,20 @@ void enterSTATE_IDLE_SAFE()
   Serial.println("STATE_IDLE_SAFE");
 #endif
 
+  if(triggerButton.isPressed())//uncock the piston
+  {
+    digitalWrite(MOTOR_PIN, HIGH);
+    tappetPlateSensorButton.waitForPress();
+    tappetPlateSensorButton.waitForRelease();
+    digitalWrite(MOTOR_PIN, LOW);
+  }
+
   display.displayRaw((const uint8_t[]){B01101101, B01110111, B01110001, B01111001});
   state = STATE_IDLE_SAFE;
+
+#ifdef MULTIMODE_BUTTON
+  currentFiremode = CURRENT_FIREMODE_SAFE;
+#endif
 }
 
 void enterSTATE_FIRING()
@@ -143,9 +183,18 @@ void enterSTATE_DISPLAYING_VOLTAGE()
 }
 // END ENTER STATES
 
+#ifdef USE_SOFTWARE_SERIAL
+  SoftwareSerial hc06(BLUETOOTH_RX_PIN, BLUETOOTH_TX_PIN);
+  #endif
+
 void setup()
 {
+  #ifdef USE_SOFTWARE_SERIAL
+  cmdInit(9600, &hc06);
+  #else
   cmdInit(9600);
+  #endif
+  
 
   cmdAdd("cfg", commandSetConfig);
   cmdAdd("testing", commandTestingMode);
@@ -212,35 +261,52 @@ void loop()
 
   case STATE_IDLE_HOT:
   case STATE_IDLE_SAFE:
-
     cmdPoll();
 
     switch (state)
     {
     case STATE_IDLE_HOT:
       // behavior
+
+#ifdef MULTIMODE_BUTTON
+      if (currentFiremode == CURRENT_FIREMODE_SAFE && currentFiremode != CURRENT_FIREMODE_2)
+#else
       if (mode1SensorButton.isPressed() && currentFiremode != CURRENT_FIREMODE_1)
+#endif
       {
         currentFiremodeRoundsNumberConfig = &fireMode1RoundsNumberConfig;
         currentFiremodeCycleConfig = &fireMode1CycleConfig;
         currentFiremode = CURRENT_FIREMODE_1;
 
+        display.displayRaw((const uint8_t[]){B00010101, B00000000, B00110000, B00000000});
+
 #ifdef TESTING
         Serial.println("CURRENT_FIREMODE_1");
 #endif
       }
+
+#ifdef MULTIMODE_BUTTON
+      if (multimodeButton.getSingleDebouncedPress() && currentFiremode == CURRENT_FIREMODE_1)
+#else
       if (mode2SensorButton.isPressed() && currentFiremode != CURRENT_FIREMODE_2)
+#endif
       {
         currentFiremodeRoundsNumberConfig = &fireMode2RoundsNumberConfig;
         currentFiremodeCycleConfig = &fireMode2CycleConfig;
         currentFiremode = CURRENT_FIREMODE_2;
 
+        display.displayRaw((const uint8_t[]){B00010101, B00000000, B01011011, B00000000});
+
 #ifdef TESTING
         Serial.println("CURRENT_FIREMODE_2");
 #endif
       }
-      // events
-      if (safetySensorButton.isPressed()) // we want to uncompress the spring and leave firing
+// events
+#ifdef MULTIMODE_BUTTON
+      if (multimodeButton.getSingleDebouncedPress() && currentFiremode == CURRENT_FIREMODE_2)
+#else
+      if (safetySensorButton.isPressed())
+#endif
       {
         enterSTATE_IDLE_SAFE();
       }
@@ -251,16 +317,24 @@ void loop()
       break;
 
     case STATE_IDLE_SAFE:
-      // behavior
-      // events
-      if (!safetySensorButton.isPressed()) // we want to uncompress the spring and leave firing
+// behavior
+// events
+#ifdef MULTIMODE_BUTTON
+      if (multimodeButton.getSingleDebouncedPress())
+#else
+      if (!safetySensorButton.isPressed())
+#endif
       {
-        if (cocked == false)
+        /*
+        if (cocked == false && tappetPlateSensorButton.isPressed())
         {
           digitalWrite(MOTOR_PIN, HIGH);
+          tappetPlateSensorButton.waitForRelease();
           tappetPlateSensorButton.waitForPress();
+          delay(PRECOCK_DELAY);
           digitalWrite(MOTOR_PIN, LOW);
-        }
+          cocked = true;
+        }*/ // TOO DANGEROUS
         enterSTATE_IDLE_HOT();
       }
       break;
@@ -269,7 +343,7 @@ void loop()
     break;
 
   case STATE_FIRING:
-    if (!tappetPlateSensorButton.isPressed()) // a bb has been fired and the sring is uncompressed
+    if (tappetPlateSensorButton.getSingleDebouncedRelease()) // a bb has been fired and the sring is uncompressed
     {
       if (shotsLeft > 0)
       {
@@ -279,20 +353,33 @@ void loop()
       cocked = false;
       //TODO : remove one BB from magazine
 
-      if (safetySensorButton.isPressed()) // we want to uncompress the spring and leave firing
+#ifdef MULTIMODE_BUTTON
+      if (multimodeButton.isPressed() && currentFiremode == CURRENT_FIREMODE_2) 
       {
         digitalWrite(MOTOR_PIN, LOW);
         enterSTATE_IDLE_SAFE();
       }
+#else
+      if (safetySensorButton.isPressed()) 
+      {
+        digitalWrite(MOTOR_PIN, LOW);
+        enterSTATE_IDLE_SAFE();
+      }
+#endif
       else // we want to precock the piston
       {
-        tappetPlateSensorButton.waitForPress();
+        while(tappetPlateSensorButton.getSingleDebouncedPress())
+        {
+
+        }
+        //tappetPlateSensorButton.waitForPress();
         cocked = true;
         if (
             !(
-                (triggerButton.isPressed() && (fullAuto || *currentFiremodeCycleConfig == FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE_AND_KEEP_ON_PRESS)) ||
+                (triggerButton.isPressed() && (fullAuto || (shotsLeft > 0 && *currentFiremodeCycleConfig == FIREMODE_CYCLE_CONFIG_STOP_IMMEDIATELY_ON_RELEASE) || *currentFiremodeCycleConfig == FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE_AND_KEEP_ON_PRESS)) || 
                 (shotsLeft > 0 && (*currentFiremodeCycleConfig == FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE || *currentFiremodeCycleConfig == FIREMODE_CYCLE_CONFIG_CONTINUE_ON_RELEASE_UNTIL_END_OF_CYCLE_AND_KEEP_ON_PRESS))))
         {
+          delay(PRECOCK_DELAY);
           digitalWrite(MOTOR_PIN, LOW);
           enterSTATE_IDLE_HOT();
         }
@@ -310,26 +397,32 @@ void commandTestingMode(int nbArgs, char **args)
 {
   if (nbArgs == 1)
   {
-    Serial.print("voltage: ");
-    Serial.println(getCurrentVoltage());
+    SERIAL_OBJECT.print("voltage: ");
+    SERIAL_OBJECT.println(getCurrentVoltage());
 
-    Serial.print("trigger button: ");
-    Serial.println(triggerButton.isPressed());
+    SERIAL_OBJECT.print("trigger button: ");
+    SERIAL_OBJECT.println(triggerButton.isPressed());
 
-    Serial.print("tappet plate sensor: ");
-    Serial.println(tappetPlateSensorButton.isPressed());
+    SERIAL_OBJECT.print("tappet plate sensor: ");
+    SERIAL_OBJECT.println(tappetPlateSensorButton.isPressed());
 
-    Serial.print("safety Sensor: ");
-    Serial.println(safetySensorButton.isPressed());
+#ifdef MULTIMODE_BUTTON
+    SERIAL_OBJECT.print("multimode button: ");
+    SERIAL_OBJECT.println(multimodeButton.isPressed());
+#else
+    SERIAL_OBJECT.print("safety sensor: ");
+    SERIAL_OBJECT.println(safetySensorButton.isPressed());
 
-    Serial.print("mode 1 Sensor: ");
-    Serial.println(mode1SensorButton.isPressed());
+    SERIAL_OBJECT.print("mode 1 Sensor: ");
+    SERIAL_OBJECT.println(mode1SensorButton.isPressed());
 
-    Serial.print("mode 2 Sensor: ");
-    Serial.println(mode2SensorButton.isPressed());
+    SERIAL_OBJECT.print("mode 2 Sensor: ");
+    SERIAL_OBJECT.println(mode2SensorButton.isPressed());
+#endif
 
     serialReturnSuccess();
-  } else
+  }
+  else
   {
     serialReturnFailure();
   }
@@ -339,41 +432,48 @@ void commandSetConfig(int nbArgs, char **args)
 {
   if (nbArgs == 3)
   {
-    if(strcmp(args[1],"vpa") == 0)
+    if (strcmp(args[1], "vpa") == 0)
     {
       voltageProtectionAlertConfig = atof(args[2]);
       EEPROM.put(EEPROM_VOLTAGE_PROTECTION_ALERT_ADRESS, voltageProtectionAlertConfig);
       serialReturnSuccess();
-    } else if(strcmp(args[1],"vpc") == 0)
+    }
+    else if (strcmp(args[1], "vpc") == 0)
     {
       voltageProtectionCutoffConfig = atof(args[2]);
       EEPROM.put(EEPROM_VOLTAGE_PROTECTION_CUTOFF_ADRESS, voltageProtectionCutoffConfig);
       serialReturnSuccess();
-    } else if(strcmp(args[1],"fm1rn") == 0)
+    }
+    else if (strcmp(args[1], "fm1rn") == 0)
     {
       fireMode1RoundsNumberConfig = atoi(args[2]);
       EEPROM.put(EEPROM_FIREMODE_1_ROUNDS_NUMBER_ADRESS, fireMode1RoundsNumberConfig);
       serialReturnSuccess();
-    } else if(strcmp(args[1],"fm2rn") == 0)
+    }
+    else if (strcmp(args[1], "fm2rn") == 0)
     {
       fireMode2RoundsNumberConfig = atoi(args[2]);
       EEPROM.put(EEPROM_FIREMODE_2_ROUNDS_NUMBER_ADRESS, fireMode2RoundsNumberConfig);
       serialReturnSuccess();
-    } else if(strcmp(args[1],"fm1c") == 0)
+    }
+    else if (strcmp(args[1], "fm1c") == 0)
     {
       fireMode1CycleConfig = atoi(args[2]);
       EEPROM.put(EEPROM_FIREMODE_1_CYCLE_ADRESS, fireMode1CycleConfig);
       serialReturnSuccess();
-    } else if(strcmp(args[1],"fm2c") == 0)
+    }
+    else if (strcmp(args[1], "fm2c") == 0)
     {
       fireMode2CycleConfig = atoi(args[2]);
       EEPROM.put(EEPROM_FIREMODE_2_CYCLE_ADRESS, fireMode2CycleConfig);
       serialReturnSuccess();
-    } else
+    }
+    else
     {
       serialReturnFailure();
     }
-  } else
+  }
+  else
   {
     serialReturnFailure();
   }
@@ -381,12 +481,12 @@ void commandSetConfig(int nbArgs, char **args)
 
 void serialReturnFailure()
 {
-  Serial.println("KO");
+  SERIAL_OBJECT.println("KO");
 }
 
 void serialReturnSuccess()
 {
-  Serial.println("OK");
+  SERIAL_OBJECT.println("OK");
 }
 
 void displayVoltage()
